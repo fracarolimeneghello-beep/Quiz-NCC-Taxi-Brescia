b0230506079223cc2905ec0be6420688e98d349b
fracarolimeneghello-beep <fracarolimeneghello@gmail.com>
2026-08-23 07:30:12 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          50: '#EEF1F6',
+          100: '#D6DDE9',
+          400: '#3C557F',
+          600: '#1D3A66',
+          700: '#17304F',
+          900: '#12233F',
+        },
+        brick: {
+          50: '#FBEAEC',
+          500: '#D6273C',
+          600: '#BE1F32',
+          700: '#9E1A2A',
+        },
+        leaf: {
+          50: '#E9F5EF',
+          500: '#1E8E5A',
+          600: '#187249',
+        },
+        paper: '#F3F5F9',
+      },
+      fontFamily: {
+        display: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"IBM Plex Sans"', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
